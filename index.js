95ebfa468fcdbb0cab1a6cdf8bf062aac898912a
Artur Shmaida <artur.shmaida@gmail.com>
2023-12-09 18:13:44 +0100

    msg

--- index.js
+++ index.js
@@ -1,19 +1,47 @@
 
 function Product(ID,name,description, price,brand,sizes,activeSize,quantity,date,reviews,images) {
+    this.ID = "";
+    this.name = "";
+    this.description = "";
+    this.price = 0.0;
+    this.brand = "";
+    this.sizes =  [];
+    this.activeSize = "";
+    this.quantity = 0;
+    this.date = new Date();
+    this.reviews =  [];
+    this.images =  [];
+}
+Product.prototype.setID = function(ID) {
     this.ID = ID;
+}
+Product.prototype.getID = function() {
+     return this.ID; 
+}
+Product.prototype.setName = function(name) {
     this.name = name;
-    this.description = description;
-    this.price =price;
-    this.brand = brand;
-    this.sizes = sizes;
-    this.activeSize = activeSize;
-    this.quantity = quantity;
-    this.date = date;
-    this.reviews =reviews;
-    this.images = images
+}
+Product.prototype.getName= function() {
+     return this.name; 
+}
+
+function Reviews(ID, author, date, comment, rating){
+    this.ID = "";
+    this.author = "";
+    this.date = new Date();
+    this.comment = "";
+    this.rating = new Map([
+        ['service', 0],
+        ['price', 0],
+        ['value', 0],
+        ['quality', 0]
+    ]);
 }
 
 
 
 let product = new Product();
-console.log(product.sizes = 11);
+product.setID(23);
+console.log(product.getID());
+
+
